e56eeaf850d0e5f9227086ace36d9f2702302e5a
Will Battel <willbattel@gmail.com>
2022-04-04 13:39:13 -0400

    Add comma-dangle and no-empty rules, version 0.3.0

--- base/index.js
+++ base/index.js
@@ -3,8 +3,14 @@ module.exports = {
         "eslint:recommended"
     ],
     rules: {
+        // Discourage dangling commas on last line of block.
+        "comma-dangle": ["warn", "always-multiline"],
+
         // Discourage use of Array constructor.
         "no-array-constructor": "warn",
+
+        // Empty blocks should be avoided but aren't otherwise problematic.
+        "no-empty": "warn",
 
         // Encourage use of constants where appropriate.
         "prefer-const": "warn",
